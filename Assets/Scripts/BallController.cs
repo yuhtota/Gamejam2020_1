555a095e7700e8cc9710abfe9d1a73d3064f8af0
yuto <yuhto.1027@gmail.com>
2020-05-16 20:57:30 +0900

    完成

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.magnitude > 8.0f)
+        {
+            rb.velocity = 8.0f * rb.velocity.normalized;
+        }
+    }
+}
---
+++ Assets/Scripts/FriendController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendController : MonoBehaviour
+{
+    public Text scoreText;
+    public Text resultText;
+    private Vector3 targetpos;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetpos = transform.position;
+        score = 0;
+        SetCountText();
+        resultText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Sin(Time.time) * 7.5f + targetpos.x, targetpos.y, targetpos.z);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            score = score + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        scoreText.text = "Count: " + score.ToString();
+
+        if (score >= 3)
+        {
+            resultText.text = "Thank You!";
+        }
+    }
+}
